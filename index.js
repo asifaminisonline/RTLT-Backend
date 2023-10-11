6b66188eec5561e494c44ada112ac8d5213b24d0
asifaminisonline <asifaminisonline@gmail.com>
2023-10-11 10:34:16 +0530

    Add GET method to retrieve data

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const locationsRouter = require("./server/routes/locations");
+// const cors = require("cors"); // Import the cors package
+
+// const app = express();
+// const port = 8000;
+
+// mongoose.connect(
+//   "mongodb+srv://asifaminisonline:wNVWMZ6VOQ3QIe8c@cluster0.qo0jfio.mongodb.net/",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
+// app.use(cors()); // Enable CORS for all origins during development
+
+// app.use(bodyParser.json());
+
+// app.use("/locations", locationsRouter);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// index.js
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -19,7 +48,8 @@ app.use(cors()); // Enable CORS for all origins during development
 
 app.use(bodyParser.json());
 
-app.use("/locations", locationsRouter);
+app.use("/locations", locationsRouter); // Use the existing route for saving location data
+// app.use("/locations", locationsRouter); // Use the new route for fetching location data
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server/routes/locations.js
+++ server/routes/locations.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Location = require("../models/Location");
 
+// GET endpoint to retrieve all location data
+router.get("/", async (req, res) => {
+  try {
+    const locations = await Location.find();
+    res.status(200).json(locations);
+  } catch (error) {
+    console.error("Error fetching location data:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+// POST endpoint to save location data
 router.post("/", async (req, res) => {
   try {
     const { latitude, longitude } = req.body;
